1b2298bb16a850722e003253a5be2db08aa2e7f4
Thuto <ThutoSiya@gmail.com>
2026-07-15 20:52:54 +0200

    Add simple concert ticket booking script with user prompts

---
+++ The Concert Ticket Booker.py
@@ -0,0 +1,10 @@
+# The Concert Ticket Booker
+# Ask the user for his/her name
+
+Name = input("Please enter your name: ")
+
+# Ask them for the name of the band/artist they want to see
+
+Artist = input("Which band or artist would you like to see perform?  ")
+
+print(f"Hey {Name}! Your tickets to see {Artist} are booked successfully!")
